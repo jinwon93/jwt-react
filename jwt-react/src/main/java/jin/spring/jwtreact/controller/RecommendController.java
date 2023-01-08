0a813a0f5c36c87cb01d0074961b785e63c10871
jinwon93 <kjw8208@nate.com>
2023-01-08 15:55:02 +0900

    controller

---
+++ jwt-react/src/main/java/jin/spring/jwtreact/controller/RecommendController.java
@@ -0,0 +1,33 @@
+package jin.spring.jwtreact.controller;
+
+
+import jin.spring.jwtreact.dto.MessageDto;
+import jin.spring.jwtreact.dto.PostRecommnedDto;
+import jin.spring.jwtreact.dto.RecommendDto;
+import jin.spring.jwtreact.service.RecommendService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/recommend")
+public class RecommendController {
+
+
+    private final RecommendService recommendService;
+
+
+    @GetMapping("/list")
+    public ResponseEntity<RecommendDto> getRecommend(@RequestParam(name = "id") Long id) {
+        return ResponseEntity.ok(recommendService.allRecommend(id));
+    }
+
+
+    @GetMapping("/")
+    public ResponseEntity<MessageDto> postRecommend(@RequestBody PostRecommnedDto dto) {
+        recommendService.createRecommend(dto.getId());
+        return ResponseEntity.ok(new MessageDto("Success"));
+    }
+}
---
+++ jwt-react/src/main/java/jin/spring/jwtreact/dto/PostRecommnedDto.java
@@ -0,0 +1,14 @@
+package jin.spring.jwtreact.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostRecommnedDto {
+
+    private Long id;
+}
